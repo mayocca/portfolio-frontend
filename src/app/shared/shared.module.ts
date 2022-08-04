c6dc1e1bb83f5c392b28d3facd7c37cc1317f15a
Matias Alejandro Yocca <5626046+mayocca@users.noreply.github.com>
2022-08-03 21:31:56 -0300

    Create footer component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
